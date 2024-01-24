473d56cb61e57f33930de6c796f384811c5cfff1
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2024-01-24 16:45:27 -0500

    Add ability for the arduino to take a user input for the final destination and then move the motor and stop when it reaches the destination.

--- Arduino/arduino_touch_share/arduino_touch_share.ino
+++ Arduino/arduino_touch_share/arduino_touch_share.ino
@@ -2,20 +2,43 @@
 #include <Stepper.h>
 
 // Defines the number of steps per rotation
-const int stepsPerRevolution = 2038;
+const int stepsPerRevolution = 2000;
+int pinPot = 0;
+int valPot = 0;
+int count = 0;
+
+int valDesired; // for incoming serial data
+
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
 Stepper myStepper = Stepper(stepsPerRevolution, 2,3,4,5);
 
 void setup() {
-    // Nothing to do (Stepper Library sets pins as outputs)
+  // Nothing to do (Stepper Library sets pins as outputs)
+  Serial.begin(115200);
+  valDesired = 0;
 }
 
 void loop() {
-	
-	// Rotate CCW quickly at 10 RPM
-	myStepper.setSpeed(10);
-	myStepper.step(stepsPerRevolution);
-	delay(1000);
+
+  if (Serial.available() > 0) {
+    valDesired = Serial.parseInt();
+    Serial.print("valDesired: ");
+    Serial.println(valDesired, DEC);
+  }
+
+  if (abs(valDesired - valPot) > 25) {
+    // Rotate CCW quickly at 10 RPM
+    myStepper.setSpeed(10);
+    myStepper.step(50);
+    count ++;
+  }
+  delay(900);
+  valPot = analogRead(0);
+  Serial.print(valPot);
+  Serial.print(" ");
+  Serial.print(count);
+  Serial.print("\n");
+  delay(100);
 }
