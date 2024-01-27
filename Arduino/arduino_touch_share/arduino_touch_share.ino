484bfc603b451a12c138de73dc51efce32cfd887
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2024-01-26 19:27:47 -0500

    Rewrite main sketch for the use of the  library. Still not using the library because need to fix #1 ( not working) first.

--- Arduino/arduino_touch_share/arduino_touch_share.ino
+++ Arduino/arduino_touch_share/arduino_touch_share.ino
@@ -1,44 +1,99 @@
-//Includes the Arduino Stepper Library
-#include <Stepper.h>
+/*
+  Functions for turning the 28BYJ-48 stepper motor with the ULN2003 
+  driver module. 
 
-// Defines the number of steps per rotation
-const int stepsPerRevolution = 2000;
-int pinPot = 0;
-int valPot = 0;
-int count = 0;
+  Written by: Arsalan Syed 
+*/
 
-int valDesired; // for incoming serial data
+#define STEPPER_PIN_1 2
+#define STEPPER_PIN_2 3
+#define STEPPER_PIN_3 4
+#define STEPPER_PIN_4 5
 
+#define MIN_TIME_ENTRE_PAS 2 // le minimum temp (en ms) entre deux pas du motor
 
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, 2,3,4,5);
+#include "StepperMotor.hpp"
+
+int step_number = 0;
+
+StepperMotor stepperMotor(STEPPER_PIN_1, STEPPER_PIN_2, STEPPER_PIN_3, STEPPER_PIN_4, MIN_TIME_ENTRE_PAS);
 
 void setup() {
-  // Nothing to do (Stepper Library sets pins as outputs)
-  Serial.begin(115200);
-  valDesired = 0;
 }
 
 void loop() {
 
-  if (Serial.available() > 0) {
-    valDesired = Serial.parseInt();
-    Serial.print("valDesired: ");
-    Serial.println(valDesired, DEC);
-  }
+  MotorSteps(1000);
+  MotorSteps(-1000);
 
-  if (abs(valDesired - valPot) > 25) {
-    // Rotate CCW quickly at 10 RPM
-    myStepper.setSpeed(10);
-    myStepper.step(50);
-    count ++;
-  }
-  delay(900);
-  valPot = analogRead(0);
-  Serial.print(valPot);
-  Serial.print(" ");
-  Serial.print(count);
-  Serial.print("\n");
-  delay(100);
 }
+
+
+void MotorSteps(int steps) {
+  int steps_abs = abs(steps);
+  int count_step = 0;
+  bool dir = (steps > 0);
+  do {
+    int step_num = count_step%4;
+    if(dir){
+    switch(step_num){
+      case 0:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+
+
+    }
+  } else {
+    switch(step_num){
+      case 0:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      } 
+    };
+    delay(MIN_TIME_ENTRE_PAS);
+    count_step++;
+  } while (count_step < steps_abs);
+}
+
