d8ee65830e2326557745f582e0418f0e78d223f2
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2024-02-17 14:33:16 -0500

    add comments about bool

--- Arduino/arduino_touch_share/StepperMotor.hpp
+++ Arduino/arduino_touch_share/StepperMotor.hpp
@@ -15,8 +15,10 @@ private:
   // step delay
 	int step_delay;
 
-  // direction TODO: to be removed
+  // direction TODO: to be removed and create local variable in functions instead
 	bool dir;
+
+  // 
 public:
 	StepperMotor(int pin1, int pin2, int pin3, int pin4, int step_delay);
 	void step(int steps);
