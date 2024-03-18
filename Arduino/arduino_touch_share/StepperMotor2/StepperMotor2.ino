78c34779d9e75a33643a62881623b193202fb07b
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2024-03-18 16:03:10 -0400

    Move from writing the pin numbers through out code to defining pinout in a seperate header file

--- Arduino/arduino_touch_share/StepperMotor2/StepperMotor2.ino
+++ Arduino/arduino_touch_share/StepperMotor2/StepperMotor2.ino
@@ -1,57 +1,81 @@
-uint32_t delayShift = 1;
+#include "pinmap.h"
+
+uint32_t delayShift = 10;
+
+bool change = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(13, OUTPUT);
-  pinMode(12, OUTPUT);
+  pinMode(SDA, OUTPUT);
+  pinMode(SCL, OUTPUT);
+  pinMode(DIR, OUTPUT);
+  digitalWrite(DIR, LOW);
+
+  // Enable pins
+  pinMode(EN1, OUTPUT);
+  digitalWrite(EN1, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //Serial.println("Hello World!");
-  motorSetup();
-}
+  motorStep();
+  delay(1000);
 
-void motorSetup(void) {
-  digitalWrite(12, LOW);
-  delay(delayShift);
-  digitalWrite(13, HIGH);
-  delay(delayShift);
-  digitalWrite(12, HIGH);
-  delay(delayShift);
-  digitalWrite(13, LOW);
-  delay(delayShift);
-  digitalWrite(12, LOW);
-  delay(delayShift);
-  digitalWrite(12, HIGH);
-  delay(delayShift);
-  digitalWrite(12, LOW);
-  delay(delayShift);
-  digitalWrite(12, HIGH);
-  delay(delayShift);
-  digitalWrite(12, LOW);
-  delay(delayShift);
-  digitalWrite(12, HIGH);
-  delay(delayShift);
+  change = !change;
+
+  if (change) digitalWrite(DIR, HIGH);
+  if (!change) digitalWrite(DIR, LOW);
 }
 
 void motorStep(void) {
-  digitalWrite(12, LOW);
+  int count = 0;
+  do {
+    motorSetup();
+    count ++;
+  } while (count < 100);
+}
+
+void motorSetup(void) {
+  digitalWrite(SCL, LOW);
   delay(delayShift);
-  digitalWrite(12, HIGH);
+  digitalWrite(SDA, HIGH);
   delay(delayShift);
-  digitalWrite(12, LOW);
+  digitalWrite(SCL, HIGH);
   delay(delayShift);
-  digitalWrite(12, HIGH);
+  digitalWrite(SDA, LOW);
   delay(delayShift);
-  digitalWrite(12, LOW);
+  digitalWrite(SCL, LOW);
   delay(delayShift);
-  digitalWrite(12, HIGH);
+  digitalWrite(SCL, HIGH);
   delay(delayShift);
-  digitalWrite(12, LOW);
+  digitalWrite(SCL, LOW);
   delay(delayShift);
-  digitalWrite(12, HIGH);
+  digitalWrite(SCL, HIGH);
   delay(delayShift);
-  digitalWrite(12, LOW);
+  digitalWrite(SCL, LOW);
+  delay(delayShift);
+  digitalWrite(SCL, HIGH);
+  delay(delayShift);
+}
+
+void motorStepSingle(void) {
+  digitalWrite(SCL, LOW);
+  delay(delayShift);
+  digitalWrite(SCL, HIGH);
+  delay(delayShift);
+  digitalWrite(SCL, LOW);
+  delay(delayShift);
+  digitalWrite(SCL, HIGH);
+  delay(delayShift);
+  digitalWrite(SCL, LOW);
+  delay(delayShift);
+  digitalWrite(SCL, HIGH);
+  delay(delayShift);
+  digitalWrite(SCL, LOW);
+  delay(delayShift);
+  digitalWrite(SCL, HIGH);
+  delay(delayShift);
+  digitalWrite(SCL, LOW);
 }
---
+++ Arduino/arduino_touch_share/StepperMotor2/pinmap.h
@@ -0,0 +1,18 @@
+#ifndef PINMAP_H
+#define PINMAP_H
+
+#define DIR 11
+#define SCL 12
+#define SDA 13
+
+#define EN1 31
+#define EN2 32
+#define EN3 33
+#define EN4 34
+
+#define VAL1 "A1"
+#define VAL2 "A2"
+#define VAL3 "A3"
+#define VAL4 "A4"
+
+#endif // PINMAP_H
