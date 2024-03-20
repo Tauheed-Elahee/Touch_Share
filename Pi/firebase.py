b1392d8ab856b51d2a217bafac05f6bb8640219c
Arsalan <arsalansyed004@gmail.com>
2024-03-20 13:41:04 -0400

    updated pyrebase, now works

--- Pi/firebase.py
+++ Pi/firebase.py
@@ -1,14 +1,14 @@
-# Lab 3 – Pyrebase example code 
+# code for communicating position from one touch share to the other 
+
 import pyrebase 
-import random 
 import time 
 import datetime
 import random
+import serial 
 
-# Config will contain the information needed to connect to your firebase 
-#   The API KEY and Project ID are found in your project settings 
-#   The DB URL can be found under the Realtime Database tab 
+mode = "r" # can be receive(r) or transmit ('t')
 
+# Config will contain the information needed to connect to the firebase
 
 config = { 
   "apiKey": "AIzaSyCoJ6WI4UIXVkrs3CrLGj7LE-7daex69kw", 
@@ -23,40 +23,65 @@ db = firebase.database()
 dataset = "touchshare1" 
 username = "arsalan" 
 
+# recieved data settings
+rec_data = [[0,0], [0,0]]
+COM_PORT = "/dev/ttyACM0"
+BAUD_RATE = 9600
 
-# get time stamp 
-ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-sensorData = ""
-
-for i in range(4):
-    for j in range(4):
-        sensorData = sensorData + "({}, {}, {})".format(i, j, random.randint(0, 255))
-
-key=1
-# When writing to your DB each child is a JSON key:value pair 
-db.child(dataset).child(ts).set(sensorData) 
+# start the serial port 
+ser = serial.Serial(COM_PORT, BAUD_RATE)
 
 
-# Next, we will retrieve the data we wrote to the DB 
-# This code will read all sensor data as a Python dictionary, 
-# convert it to a list, extract the final entry, and print its  
-# key and value pair 
+# message to be sent to the arduino
+last_msg_to_arduino = ""
 
-mySensorData = db.child(username).child(dataset).get() 
+while(True):
+    
+    
+    
+    if(mode == 'r'):
+        # get the data from firebase 
+        mySensorData = db.child(dataset).get() 
 
-#print("Parent Key:{}".format(mySensorData.key())) 
-#print("Parent Value: {}".format(mySensorData.val()))   
+        # convert data into a dictionary 
+        mySensorData_dict = mySensorData.val() 
 
-# Returns the dictionary as a list 
-mySensorData_list = mySensorData.each() 
+        # get time that the sensor data was taken 
+        time_stamp = list(mySensorData_dict.keys())[-1]
 
-# Takes the last element of the list 
-#lastDataPoint = mySensorData_list[-1] 
-#print ("three most recent measurements: \n")
-#print ("recorded at {}: {}".format(mySensorData_list[-2].key(), mySensorData_list[-2].val()))
-#print ("recorded at {}: {}".format(mySensorData_list[-3].key(), mySensorData_list[-3].val()))
-#print ("recorded at {}: {}".format(mySensorData_list[-4].key(), mySensorData_list[-4].val()))
- 
-#print("Child Key: {}".format(lastDataPoint.key())) 
-#print("Child Value: {}\n".format(lastDataPoint.val())) 
+        # get the most recent data for the nodes
+        all_data = mySensorData_dict[time_stamp]
+
+        if(last_msg_to_arduino != all_data):
+            # send the data out of the serial port 
+            ser.write(all_data.encode())
+
+            print("sent: {}".format(all_data))
+
+            last_msg_to_arduino = all_data
+
+        else:
+            print("nothing new to send")
+
+
+    ## ================== ##
+    ## WRITE TO FIREBASE  ## 
+    ## ================== ##
+
+    elif(mode == 't'):
+        
+        # Read a line of text from the serial port
+        line = "1,2,3,4"#= ser.readline().decode().strip()
+        
+        # Check if the line is not empty
+        if line:
+            print("Received:", line)  # Print the received line
+
+        # get time stamp
+        ts = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        # upload to firebase 
+        db.child(dataset).child(ts).set(line) 
+
+    # wait one second 
+    time.sleep(1)
